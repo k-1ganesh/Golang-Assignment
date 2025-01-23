749a059773c6044d476971d02d736fbbfb348b98
Ganesh <kaganesh12@gmail.com>
2025-01-23 21:00:10 +0530

    Add code to handle error

--- Q1.go
+++ Q1.go
@@ -16,14 +16,38 @@ import (
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Enter the value of Principle,Rate,Time")
-	input,_ := reader.ReadString('\n')
+	input,err := reader.ReadString('\n')
+	if err!=nil{
+		fmt.Errorf("error in reading input")
+		return
+	}
+	
 	input = strings.TrimSpace(input)
 
 	values := strings.Split(input , ",")
 
-	Principal,_ := strconv.ParseFloat(values[0] , 64)
-	Rate,_ := strconv.ParseFloat(values[1] , 64)
-	Time,_ := strconv.ParseFloat(values[2] , 64)
+	if len(values) != 3 {
+		fmt.Println("Enter the valid input")
+		return
+	}
+
+	Principal,err:= strconv.ParseFloat(values[0] , 64)
+	if err != nil {
+		fmt.Errorf("Not a Number")
+		return
+	}
+
+	Rate,err := strconv.ParseFloat(values[1] , 64)
+	if err != nil {
+		fmt.Errorf("Not a Number")
+		return
+	}
+
+	Time,err:= strconv.ParseFloat(values[2] , 64)
+	if err != nil {
+		fmt.Errorf("Not a Number")
+		return
+	}
 
 	Compound_Interest := Principal * Rate * Time / 100 
 	fmt.Printf("Compound Interest is: %.2f" , Compound_Interest)
