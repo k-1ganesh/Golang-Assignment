424af07898e8ac76ab12ad8d31d55afbb2bb0e2e
Ganesh <kaganesh12@gmail.com>
2025-01-25 17:37:10 +0530

    Solve assignment 4

---
+++ A4Q1.go
@@ -0,0 +1,63 @@
+/* 1. The given program accepts an integer value between 1 to 4 from the user.
+There is an option associated with each value, which is basically objects of different data types with some associated value.
+
+Write a  method named "AcceptAnything" which takes any type of data as input.
+
+Based on the option chosen by the user, we will send different types of objects to this function and it should print the following based on its respective data type of value sent to the function "AcceptAnything".
+
+integer :
+"This is a value of type Integer, <value>"
+
+string :
+"This is a value of type String, <value>"
+
+boolean :
+"This is a value of type Boolean, <value>"
+
+Custom data type Hello :
+"This is a value of type Hello, <value>"
+*/
+
+package main
+
+import "fmt"
+
+// Empty interface can have any value.
+func AcceptAnything(val interface{}) {
+
+	switch v := val.(type) {
+	case int:
+		fmt.Printf("This is the value of type Integer , %v", v)
+	case string:
+		fmt.Printf("This is the value of type String , %v", v)
+	case bool:
+		fmt.Printf("This is the value of type Boolean , %v", v)
+	case Student:
+		fmt.Printf("This is the value of type Student , %v", v)
+	default:
+		fmt.Println("Value of Unknown type is passed.")
+	}
+
+}
+
+type Student struct {
+	name string
+}
+
+func main() {
+	fmt.Println("Enter the input 1 to 4")
+	var input int
+	fmt.Scanln(&input)
+
+	if input == 1 {
+		AcceptAnything(10)
+	} else if input == 2 {
+		AcceptAnything("Ganesh")
+	} else if input == 3 {
+		AcceptAnything(true)
+	} else if input == 4 {
+		AcceptAnything(Student{name: "Ganesh"})
+	} else {
+		fmt.Println("Enter valid input")
+	}
+}
