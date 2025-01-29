c0e36790608de89330449ade2a888c419246065a
Ganesh <kaganesh12@gmail.com>
2025-01-29 15:26:54 +0530

    Solve assignment 7

--- A7Q1.go
+++ A7Q1.go
@@ -1,10 +1,12 @@
-# Golang-Assignment
-```go
+// Reason why code was failing because of irregularities between the execution of both ananomous function.
+// For consistent output the function incrementing n should be executed after the function checking n is odd or even.
+
 package main
 
 import (
 	"fmt"
 	"time"
+	"sync"
 )
 
 func isEven(n int) bool {
@@ -13,8 +15,10 @@ func isEven(n int) bool {
 
 func main() {
 	n := 0
-
+    var wg sync.WaitGroup
 	go func() {
+		defer wg.Done() 
+		wg.Add(1)
 		nIsEven := isEven(n)
 		time.Sleep(5 * time.Millisecond)
 		if nIsEven {
@@ -25,10 +29,10 @@ func main() {
 	}()
 
 	go func() {
+		wg.Wait() // This blocks function until above function completes its execution.
 		n++
 	}()
 
 	// just waiting for the goroutines to finish before exiting
 	time.Sleep(time.Second)
 }
-```
